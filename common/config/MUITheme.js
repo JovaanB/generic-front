17db3a8da49b77e6f9894f8fb2b18caca3d3e150
Jovan <bienvenujovan@gmail.com>
2022-02-23 18:39:33 +0100

    MUI cache & responsiveFontSizes

--- common/config/MUITheme.js
+++ common/config/MUITheme.js
@@ -1,10 +1,12 @@
-import { createTheme } from "@mui/material/styles";
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 import { primaryColor } from "./constants";
 
-const MUITheme = createTheme({
+let MUITheme = createTheme({
     palette: {
         primary: { main: primaryColor },
     },
 });
+
+MUITheme = responsiveFontSizes(MUITheme);
 
 export default MUITheme;
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,27 @@
+import Head from "next/head";
 import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider } from "@emotion/react";
 
 import MUITheme from "../common/config/MUITheme";
+import createEmotionCache from "../styles/createEmotionCache";
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }) {
+const clientSideEmotionCache = createEmotionCache();
+
+const MyApp = ({ Component, emotionCache = clientSideEmotionCache, pageProps }) => {
     return (
-        <ThemeProvider theme={MUITheme}>
-            <Component {...pageProps} />
-        </ThemeProvider>
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <title>SportinTech</title>
+                <meta name="viewport" content="initial-scale=1, width=device-width" />
+            </Head>
+            <ThemeProvider theme={MUITheme}>
+                <CssBaseline />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </CacheProvider>
     );
-}
+};
 
 export default MyApp;
